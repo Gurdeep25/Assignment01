2999f717f7a27640d683202d3993d1ce79eb3657
Gurdeep25 <gscs.engg@gmail.com>
2020-02-06 23:40:30 -0500

    Final commit

---
+++ Assignment01.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Assignment01.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assignment01.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e19130f7-9a19-4407-b12b-bf54b9a58104")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Assignment01.Tests/RectangleTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace Assignment01.Tests
+{
+    [TestFixture]
+    public class RectangleTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Rectangle r = new Rectangle();
+        }
+
+        [Test]
+        public void GetLength_Input1_Output1()
+        {
+            Rectangle r = new Rectangle();
+            Assert.AreEqual(1, r.GetLength(), 0);
+        }
+        [Test]
+        public void GetWidth_Input1_Output1()
+        {
+            Rectangle r = new Rectangle();
+            Assert.AreEqual(1, r.GetWidth(), 0);
+        }
+        [Test]
+        public void SetLength_UserInput2_Output2()
+        {
+            Rectangle r = new Rectangle();
+            Assert.AreEqual(2, r.SetLength(2), 0);
+        }
+        [Test]
+        public void SetWidth_UserInput3_Output3()
+        {
+            Rectangle r = new Rectangle();
+            Assert.AreEqual(3, r.SetWidth(3), 0);
+        }
+        [Test]
+        public void GetPerimeter_UserInputLength2Width2_Output8()
+        {
+            Rectangle r = new Rectangle(2,2);
+            Assert.AreEqual(8, r.GetPerimeter(), 0);
+        }
+        [Test]
+        public void GetArea_UserInputLength2Width2_Output4()
+        {
+            Rectangle r = new Rectangle(2, 2);
+            Assert.AreEqual(4, r.GetArea(), 0);
+        }
+
+    }
+}
